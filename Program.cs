d199351ccae3fd0238c0619fcb0317a35c686c6e
xseln <123553271+xseln@users.noreply.github.com>
2024-07-10 14:06:05 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace RestorantManagement
+{
+    public class Table
+    {
+        public int TableId { get; set; }
+        public int Capacity { get; set; }
+        public bool IsReserved { get; set; }
+        public string ReservationName { get; set; }
+    }
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+          
+        }
+      
+    }
+}
